f40e669bcd4f16d7b5ac460f7d0b2e67bea107b3
rankoneprogressive@gmail.com <rankoneprogressive@gmail.com>
2019-02-27 10:03:55 +0200

    lab

---
+++ laborator1/laborator1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laborator1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            point p = new point();
+
+        }
+    }
+}
+//o clasa point  
---
+++ laborator1/laborator1/point.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace laborator1
+{
+    internal class point
+    {
+        double x, y;
+
+        public point(double a, double b)
+        {
+            x = a;
+            y = b;
+        }
+        public double X
+        {
+            get;
+        }
+        public double Y
+        {
+            get;
+        }
+        public point()
+            {
+            x = 0;
+            y = 0;
+            }
+        public void  rotate (int t )
+        {
+            double x0, y0;
+            t = t * Math.PI / 180; 
+            x0 = x;
+            y0 = y;
+            x = x0 * Math.Cos(t) - y0 * Math.Asin(t);
+            y = y0 * Math.Cos(t) + x0 * Math.Asin(t);
+        }
+
+    }
+}
+//
